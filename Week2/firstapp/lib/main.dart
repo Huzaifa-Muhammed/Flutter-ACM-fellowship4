ec037719d082e6814de028813ce23a1b86fc9d20
Muhammad Huzaifa <muhammadhuzaifamh777@gmail.com>
2023-08-24 20:54:58 +0500

    practied all type of images in flutter i.e background,url,local

--- Week2/firstapp/lib/main.dart
+++ Week2/firstapp/lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main()
 {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key }) :super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +16,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       darkTheme:ThemeData(primarySwatch: Colors.grey) ,
       themeMode: ThemeMode.system ,
-      home: const MyHomePage()
+      home:MyHomePage()
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
 
-  const MyHomePage({Key? key }) :super(key: key);
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -41,34 +38,47 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Colors.green,
           centerTitle: true,
         ),
-      body: Center(
-        child: Container(
-          width: 200,
-          height: 200, 
-          color: Colors.lightGreen,
-          child: const Center(
-              child:Text(
-                'hello',
-                style:TextStyle(
-                  fontSize: 20,
-                  letterSpacing: 2,
-                  color: Colors.deepOrangeAccent,
-                  height: 10,
-                  wordSpacing: 3,
-                  fontFamily: 'cursive',
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW5pbWF0ZWQlMjBtb3JuaW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'),
+            fit: BoxFit.scaleDown,
+            repeat: ImageRepeat.repeatY,
+          )
+        ),
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image(
+                image:NetworkImage('https://images.unsplash.com/photo-1692545052139-09d7b8fef762?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'),
+                width: 200,
+                height: 200,
+                fit: BoxFit.fill,
+              ),
+              Image(
+                image:AssetImage('assets/images/solider.png'),
+                width:150 ,
+                height: 250,
+                fit: BoxFit.fill,
+              ),
+              CircleAvatar(
+                backgroundImage: NetworkImage('https://images.unsplash.com/photo-1692317023059-499bf304ef55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8SnBnNktpZGwtSGt8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'),
+                radius: 100,
+              ),
+              CircleAvatar(
+                child: ClipOval(
+                  child: Image(
+                    image:AssetImage('assets/images/solider.png'),
+                    fit:BoxFit.fill ,
+                    width: 500,
+                    height: 500,
+                  ),
                 ),
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                textHeightBehavior: TextHeightBehavior(applyHeightToFirstAscent: true),
               )
+            ],
           ),
         ),
-      ),
-      floatingActionButton:FloatingActionButton(
-        elevation: 20.0,
-        child:const Icon(Icons.call_end_rounded),
-        onPressed: (){
-        },
       )
     );
   }
