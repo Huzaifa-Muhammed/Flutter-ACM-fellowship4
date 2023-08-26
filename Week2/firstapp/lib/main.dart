097facd92ba0ee93f7a8255a017682d253afce46
Muhammad Huzaifa <muhammadhuzaifamh777@gmail.com>
2023-08-26 22:49:12 +0500

    practiced google fonts and setup .yaml for google fonts

--- Week2/firstapp/lib/main.dart
+++ Week2/firstapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main()
 {
@@ -34,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
         appBar: AppBar(
-          title:const Text('My First App'),
+          title:Text('My First App',style:GoogleFonts.aboreto()),
           backgroundColor: Colors.green,
           centerTitle: true,
         ),
@@ -42,39 +43,56 @@ class _MyHomePageState extends State<MyHomePage> {
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: NetworkImage('https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW5pbWF0ZWQlMjBtb3JuaW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'),
-            fit: BoxFit.scaleDown,
-            repeat: ImageRepeat.repeatY,
-          )
+            fit: BoxFit.cover,
+        )
         ),
-        child: const Center(
-          child: Column(
+        child: Center(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image(
-                image:NetworkImage('https://images.unsplash.com/photo-1692545052139-09d7b8fef762?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'),
-                width: 200,
-                height: 200,
-                fit: BoxFit.fill,
-              ),
-              Image(
-                image:AssetImage('assets/images/solider.png'),
-                width:150 ,
-                height: 250,
-                fit: BoxFit.fill,
-              ),
-              CircleAvatar(
-                backgroundImage: NetworkImage('https://images.unsplash.com/photo-1692317023059-499bf304ef55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8SnBnNktpZGwtSGt8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'),
-                radius: 100,
-              ),
-              CircleAvatar(
-                child: ClipOval(
-                  child: Image(
-                    image:AssetImage('assets/images/solider.png'),
-                    fit:BoxFit.fill ,
-                    width: 500,
-                    height: 500,
+              const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(
+                    image:NetworkImage('https://images.unsplash.com/photo-1692545052139-09d7b8fef762?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'),
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.fill,
                   ),
-                ),
+                  Image(
+                    image:AssetImage('assets/images/solider.png'),
+                    width:150 ,
+                    height: 250,
+                    fit: BoxFit.fill,
+                  ),
+                  CircleAvatar(
+                    backgroundImage: NetworkImage('https://images.unsplash.com/photo-1692317023059-499bf304ef55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8SnBnNktpZGwtSGt8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'),
+                    radius: 50,
+                  ),
+                ],
+              ),
+              const Column(
+                children: [
+                  CircleAvatar(
+                    radius: 100,
+                    child: ClipOval(
+                      child: Image(
+                        image:AssetImage('assets/images/solider.png'),
+                        fit:BoxFit.fill ,
+                        width: 150,
+                        height: 200,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Hello',style: GoogleFonts.abhayaLibre(fontSize: 40),),
+                  Text('Hello',style: GoogleFonts.alegreyaSans(fontSize: 40),),
+                  Text('Hello',style: GoogleFonts.ballet(fontSize: 40),)
+                ],
               )
             ],
           ),
