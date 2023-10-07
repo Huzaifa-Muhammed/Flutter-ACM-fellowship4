9bd01f3ee8239fc024f76f0c944a2ab6e63504c9
Muhammad Huzaifa <muhammadhuzaifamh777@gmail.com>
2023-10-07 16:24:03 +0500

    understand async and await in dart

--- Week 1/main.dart
+++ Week 1/main.dart
@@ -6,3 +6,24 @@ void main() {
   String input = stdin.readLineSync();
   stdout.writeln(input);
 }
+
+
+void main() async {
+  futureFunction();   // will execute this lines and when this function's time completed then print this function
+  var myMap = Map();
+  myMap = {651: "Huzaifa", 657: "Mahad", 683: "Faizan"};
+  print(myMap[651]);
+  await listDisplay();  // will wait for this funtion's delay to be completed then execute next line "due to await"
+  print(myMap[683]);
+}
+
+Future futureFunction() async {
+  Future.delayed(Duration(seconds: 1)).whenComplete(() => print("Future done"));
+}
+
+Future listDisplay() async {
+  var myList = ["Huzaifa from List", "Mahad", "Faizan"];
+  myList.add("Huziafa");
+  await Future.delayed(Duration(seconds: 3))
+      .whenComplete(() => print(myList[0]));
+}
